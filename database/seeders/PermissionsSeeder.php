3fecf48ed35413ade07d42ae8f7806e42ed3bc38
ali <alila3883@gmail.com>
2021-06-15 12:18:09 +0300

    translate Permission dumy data

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -13,33 +13,39 @@ class PermissionsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('permissions')->insert(['name' => 'view-permissions', 'description' => 'عرض الصلاحيات', 'message' => 'غير مصرح لك عرض الصلاحيات']);
+        DB::table('permissions')->insert(['name' => 'view-permission', 'description' => 'view permission', 'message' => 'You dont allow to view permission']);
+        DB::table('permissions')->insert(['name' => 'add-permission', 'description' => 'add permission', 'message' => 'You dont allow to add permission']);
+        DB::table('permissions')->insert(['name' => 'edit-permission', 'description' => 'edit permission', 'message' => 'You dont allow to edit permission']);
+        DB::table('permissions')->insert(['name' => 'delete-permission', 'description' => 'delete permission', 'message' => 'You dont allow to delete permission']);
 
-        DB::table('permissions')->insert(['name' => 'add-building', 'description' => 'إضافة مباني', 'message' => 'غير مصرح لك إضافة مباني']);
-        DB::table('permissions')->insert(['name' => 'add-building', 'description' => 'إضافة مباني', 'message' => 'غير مصرح لك إضافة مباني']);
-        DB::table('permissions')->insert(['name' => 'edit-building', 'description' => 'تعديل مبنى', 'message' => 'غير مصرح لك تعديل مبنى']);
-        DB::table('permissions')->insert(['name' => 'delete-building', 'description' => 'حذف مبنى', 'message' => 'غير مصرح لك حذف مبنى']);
+        DB::table('permissions')->insert(['name' => 'view-building', 'description' => 'view building', 'message' => 'You dont allow to view building']);
+        DB::table('permissions')->insert(['name' => 'add-building', 'description' => 'add building', 'message' => 'You dont allow to add building']);
+        DB::table('permissions')->insert(['name' => 'edit-building', 'description' => 'edit building', 'message' => 'You dont allow to edit building']);
+        DB::table('permissions')->insert(['name' => 'delete-building', 'description' => 'delete building', 'message' => 'You dont allow to delete building']);
 
-        DB::table('permissions')->insert(['name' => 'add-extinguisher', 'description' => 'إضافة طفاية', 'message' => 'غير مصرح لك إضافة طفاية']);
-        DB::table('permissions')->insert(['name' => 'edit-extinguisher', 'description' => 'تعديل طفاية', 'message' => 'غير مصرح لك تعديل طفاية']);
-        DB::table('permissions')->insert(['name' => 'delete-extinguisher', 'description' => 'حذف طفاية', 'message' => 'غير مصرح لك حذف طفاية']);
+        DB::table('permissions')->insert(['name' => 'view-extinguisher', 'description' => 'view extinguisher', 'message' => 'You dont allow to view an extinguisher']);
+        DB::table('permissions')->insert(['name' => 'add-extinguisher', 'description' => 'add extinguisher', 'message' => 'You dont allow to add an extinguisher']);
+        DB::table('permissions')->insert(['name' => 'edit-extinguisher', 'description' => 'edit extinguisher', 'message' => 'You dont allow to edit an extinguisher']);
+        DB::table('permissions')->insert(['name' => 'delete-extinguisher', 'description' => 'delete extinguisher', 'message' => 'You dont allow to delete an extinguisher']);
 
-        DB::table('permissions')->insert(['name' => 'add-page', 'description' => 'إضافة صفحة', 'message' => 'غير مصرح لك إضافة صفحة']);
-        DB::table('permissions')->insert(['name' => 'edit-page', 'description' => 'تعديل صفحة', 'message' => 'غير مصرح لك تعديل صفحة']);
-        DB::table('permissions')->insert(['name' => 'delete-page', 'description' => 'حذف صفحة', 'message' => 'غير مصرح لك حذف صفحة']);
+        DB::table('permissions')->insert(['name' => 'view-page', 'description' => 'view page', 'message' => 'You dont allow to view page']);
+        DB::table('permissions')->insert(['name' => 'add-page', 'description' => 'add page', 'message' => 'You dont allow to add page']);
+        DB::table('permissions')->insert(['name' => 'edit-page', 'description' => 'edit page', 'message' => 'You dont allow to edit page']);
+        DB::table('permissions')->insert(['name' => 'delete-page', 'description' => 'delete page', 'message' => 'You dont allow to delete page']);
 
-        DB::table('permissions')->insert(['name' => 'add-user', 'description' => 'إضافة المستخدمين', 'message' => 'غير مصرح لك إضافة المستخدمين']);
-        DB::table('permissions')->insert(['name' => 'edit-user', 'description' => 'تعديل بيانات المستخدمين', 'message' => 'غير مصرح لك تعديل بيانات المستخدمين']);
-        DB::table('permissions')->insert(['name' => 'delete-user', 'description' => 'حذف المستخدمين', 'message' => 'غير مصرح لك حذف المستخدمين']);
-        DB::table('permissions')->insert(['name' => 'restore-user', 'description' => 'استعادة مستخدم محذوف', 'message' => 'غير مصرح لك استعادة مستخدم محذوف']);
-        DB::table('permissions')->insert(['name' => 'force-delete-user', 'description' => 'حذف مستخدم نهائياً', 'message' => 'غير مصرح لك حذف مستخدم نهائياً']);
+        DB::table('permissions')->insert(['name' => 'view-user', 'description' => 'view user', 'message' => 'You dont allow to view user']);
+        DB::table('permissions')->insert(['name' => 'add-user', 'description' => 'add user', 'message' => 'You dont allow to add user']);
+        DB::table('permissions')->insert(['name' => 'edit-user', 'description' => 'edit user', 'message' => 'You dont allow to edit user']);
+        DB::table('permissions')->insert(['name' => 'delete-user', 'description' => 'delete user', 'message' => 'You dont allow to delete user']);
+        DB::table('permissions')->insert(['name' => 'restore-user', 'description' => 'restore user', 'message' => 'You dont allow to restore user']);
+        DB::table('permissions')->insert(['name' => 'force-delete-user', 'description' => 'force delete userً', 'message' => 'You dont allow to force delete user']);
 
-        DB::table('permissions')->insert(['name' => 'delete-contact', 'description' => 'حذف الرسالةً', 'message' => 'غير مصرح لك حذف الرسالةً']);
-        DB::table('permissions')->insert(['name' => 'view-contacts', 'description' => 'عرض الرسالةً', 'message' => 'غير مصرح لك عرض الرسالةً']);
+        DB::table('permissions')->insert(['name' => 'view-contact', 'description' => 'view contact', 'message' => 'You dont allow to view contact']);
+        DB::table('permissions')->insert(['name' => 'delete-contact', 'description' => 'delete contact', 'message' => 'You dont allow to delete contact']);
 
-        DB::table('permissions')->insert(['name' => 'show-link', 'description' => 'عرض الرابط', 'message' => 'غير مصرح لك عرض الرابط']);
-        DB::table('permissions')->insert(['name' => 'add-link', 'description' => 'اضافة الرابط', 'message' => 'غير مصرح لك اضافة الرابط']);
-        DB::table('permissions')->insert(['name' => 'edit-link', 'description' => 'تعديل الرابط', 'message' => 'غير مصرح لك تعديل الرابط']);
-        DB::table('permissions')->insert(['name' => 'delete-link', 'description' => 'حذف الرابط', 'message' => 'غير مصرح لك حذف الرابط']);
+        DB::table('permissions')->insert(['name' => 'viw-link', 'description' => 'viw link', 'message' => 'You dont allow to viw link']);
+        DB::table('permissions')->insert(['name' => 'add-link', 'description' => 'add link', 'message' => 'You dont allow to add link']);
+        DB::table('permissions')->insert(['name' => 'edit-link', 'description' => 'edit link', 'message' => 'You dont allow to edit link']);
+        DB::table('permissions')->insert(['name' => 'delete-link', 'description' => 'delete link', 'message' => 'You dont allow to delete link']);
     }
 }
